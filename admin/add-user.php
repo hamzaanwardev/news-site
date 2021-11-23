b26f5dcebf7702d722c655329943fb8d3d4fcd9f
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-24 00:33:51 +0500

    Update add-user.php

--- admin/add-user.php
+++ admin/add-user.php
@@ -7,7 +7,7 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
-                  <form  action="" method ="POST" autocomplete="off">
+                  <form  action="<?php $_SERVER['PHP_SELF']; ?>" method ="POST" autocomplete="off">
                       <div class="form-group">
                           <label>First Name</label>
                           <input type="text" name="fname" class="form-control" placeholder="First Name" required>
