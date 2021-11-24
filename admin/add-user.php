1fba6948f82d0f43071e85ab06eb60036644eac8
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-24 23:12:25 +0500

    Making Table rows dynamic

--- admin/add-user.php
+++ admin/add-user.php
@@ -4,21 +4,21 @@ if(isset($_POST['save'])){
   include "config.php";
 
   $fname =mysqli_real_escape_string($conn,$_POST['fname']);
-  $lname = mysqli_real_escape_string($conn,$_POST['lfname']);
+  $lname = mysqli_real_escape_string($conn,$_POST['fname']);
   $user =mysqli_real_escape_string($conn,$_POST['user']);
   $password = mysqli_real_escape_string($conn,md5($_POST['password']));
   $role = mysqli_real_escape_string($conn,$_POST['role']);
 
-  $sql = "SELECT username FROM WHERE username = {$user}";
+ $sql = "SELECT username FROM user WHERE username = '{$user}'";
   $result = mysqli_query($conn, $sql) or die("Query Failed."); 
 
   if(mysqli_num_rows($result) > 0){
-    echo "<p style='color:red;text-align:center;margin: 10px 0;'>UserName already exists.</p>"
+    echo "<p style='color:red;text-align:center;margin: 10px 0;'>UserName already exists.</p>";
   }else{
     $sql1 = "INSERT INTO user (first_name, last_name, username, password, role)
     VALUES ('{$fname}','{$lname}','{$user}','{$password}','{$role}')";
-    if(mysqli_query($conn,$sqli1)){
-      header("Location: http://localhost/news-site/admin/user.php");
+    if(mysqli_query($conn,$sql1)){
+      header("Location: {$hostname}/admin/users.php");
     }
   }
 }
