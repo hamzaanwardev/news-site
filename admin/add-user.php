269ab19dc86a7fc221ac5c0759559b6dd6f74a57
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-24 22:25:10 +0500

    Update add-user.php

--- admin/add-user.php
+++ admin/add-user.php
@@ -12,7 +12,15 @@ if(isset($_POST['save'])){
   $sql = "SELECT username FROM WHERE username = {$user}";
   $result = mysqli_query($conn, $sql) or die("Query Failed."); 
 
-  if(mysqli_num_rows)
+  if(mysqli_num_rows($result) > 0){
+    echo "<p style='color:red;text-align:center;margin: 10px 0;'>UserName already exists.</p>"
+  }else{
+    $sql1 = "INSERT INTO user (first_name, last_name, username, password, role)
+    VALUES ('{$fname}','{$lname}','{$user}','{$password}','{$role}')";
+    if(mysqli_query($conn,$sqli1)){
+      header("Location: http://localhost/news-site/admin/user.php");
+    }
+  }
 }
 
 ?> 
