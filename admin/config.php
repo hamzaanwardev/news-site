89fcb50659c860c37b7b6b4de7aed8905ee750bd
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-24 23:37:02 +0500

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,5 +1,5 @@
 <?php
-$hostname = "http://localhost/news-site/Git";
+$hostname = "http://localhost/Git/news-site";
 
 $conn = mysqli_connect("localhost","root","","news-site-db") or die("Connection failed : " . mysqli_connect_error());
 
