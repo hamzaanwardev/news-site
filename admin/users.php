b497baf36dbfaac6b1f0c11bd6131470bec46348
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-11-24 23:59:01 +0500

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -37,7 +37,7 @@
                               }else{
                                   echo "Normal";
                               } ?></td>
-                              <td class='edit'><a href='update-user.php?id={}'><i class='fa fa-edit'></i></a></td>
+                              <td class='edit'><a href='update-user.php?id={$row["user_id"]}'><i class='fa fa-edit'></i></a></td>
                               <td class='delete'><a href='delete-user.php?id={}'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           <?php } ?>
