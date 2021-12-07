ab459fa91cd992c782b56e6e2c38e62060d68d5f
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-12-07 09:12:07 +0500

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -36,20 +36,26 @@
                                 $username = mysqli_real_escape_string($conn, $_POST['username']);
                                 $password = md5($_POST['password']);
 
-                                $sql = "SELECT user_id, username, role FROM user WHERE username = {'$username'} AND password = {'$password'}";
+                                $sql = "SELECT user_id, username, role FROM user WHERE username = '{$username}' AND password = '{$password}'";
+                                
                                 $result = mysqli_query($conn, $sql) or die ("Query Failed.");
 
                                 if(mysqli_num_rows($result) > 0) {
 
                                     while($row = mysqli_fetch_assoc($result)) {
                                         session_start();
+                                        $_SESSION["username"] = $row['username'];
+                                        $_SESSION["user_id"] = $row['user_id'];
+                                        $_SESSION["user_role"] = $row['role'];
+
+
+                                        header("Location: {$hostname}/admin/post.php");
                                     }
 
                                 }else{
                                     echo '<div class="alert alert-danger">Username and Password ae not matched.</div>';
                                 }
                             }
-
                         ?>
                     </div>
                 </div>
