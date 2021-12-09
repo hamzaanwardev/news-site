76a04b63f0821e577f95b1634877bb5f108c29c1
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-12-09 23:58:26 +0500

    Update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -20,6 +20,9 @@
                           <label for="exampleInputPassword1">Category</label>
                           <select name="category" class="form-control">
                               <option value="" selected> Select Category</option>
+                              <?php
+    include config.php;
+                              ?>
                           </select>
                       </div>
                       <div class="form-group">
