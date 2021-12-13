941e161edb885765ca9164a2a979dd30225d705c
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-12-13 23:57:17 +0500

    Update add-post.php

--- admin/add-post.php
+++ admin/add-post.php
@@ -28,7 +28,7 @@
 
                                    if(mysqli_num_rows($result) > 0) {
                                        while($row = mysqli_fetch_assoc($result)) {
-                                           echo ""
+                                           echo "<option></option>";
                                        }
                                    }
                               ?>
